999e751e166f3899d1fff9f5974c319f4eff0cda
Beyza Koroglu <beyza.koroglu.97@gmail.com>
2022-02-09 21:25:15 +0300

    added filters.py

---
+++ vetApp/pets/filters.py
@@ -0,0 +1,17 @@
+from django_filters import rest_framework as filters
+
+from pets.models import PetOwner, Pet
+
+
+class PetOwnerFilter(filters.FilterSet):
+
+    class Meta:
+        model = PetOwner
+        fields = ("full_name",)
+
+
+class PetFilter(filters.FilterSet):
+
+    class Meta:
+        model = Pet
+        fields = ("owner__full_name", "name")
--- vetApp/pets/views.py
+++ vetApp/pets/views.py
@@ -3,12 +3,16 @@ from rest_framework import viewsets
 from pets.models import Pet, PetOwner
 from pets.serializers import PetOwnerSerializer, PetSerializer
 
+from pets.filters import PetOwnerFilter, PetFilter
+
 
 class PetOwnerViewSet(viewsets.ModelViewSet):
     queryset = PetOwner.objects.all()
     serializer_class = PetOwnerSerializer
+    filterset_class = PetOwnerFilter
 
 
 class PetViewSet(viewsets.ModelViewSet):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+    filterset_class = PetFilter
